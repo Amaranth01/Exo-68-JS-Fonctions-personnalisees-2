d7f4dbd370ff0a85fb4d4fa5d27ad42924caf0da
Amaranth01 <vanessa.nectoux@gmail.com>
2021-11-17 11:06:02 +0100

    Exo 68 ok

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 let a = document.getElementsByTagName('p');
 
-function randomNumber (max) {
+function randomNumber () {
 
     return Math.trunc(Math.random()*100);
 }
-
-let b = randomNumber()
 
 for (let i = 0; i < 10; i++) {
    a[i].innerHTML = randomNumber().toString();
